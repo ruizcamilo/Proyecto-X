10da0dd8700311e8997e176bec8a729595add901
nf.abondano <nf.abondano@uniandes.edu.co>
2020-06-26 12:34:29 -0500

    Enemies semicorrection - tilemap snow

---
+++ Assets/Scripts/Enemies/ActivateLadder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateLadder : MonoBehaviour
+{
+	public Transform otherPoint;
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Enemy" && other.GetComponent<SkeletonPatroling>() != null)
+        {
+			other.GetComponent<SkeletonPatroling>().moveInLadder(otherPoint);
+		}
+	}
+}
---
+++ Assets/Scripts/Enemies/SkeletonPatroling.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonPatroling : MonoBehaviour
+{
+    public float speed = 10f;
+    public GameObject first;
+    public GameObject second;
+    public float waitingTime = 2f;
+    public float reactRange = 1f;
+    public float chaseRange = 4f;
+
+    private GameObject _target;
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+    private Transform _ladderTarget;
+    private Transform _player;
+    private bool _isReacting = false;
+    private bool _isChasing = false;
+    private bool _isAttacking = false;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        if(GameObject.FindGameObjectWithTag("Player") != null)
+            _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateDirection();
+        StartCoroutine("PatrolToTarget");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_player != null && !_isChasing && Vector2.Distance(transform.position, _player.position) < reactRange)
+        {
+            if (!_isReacting)
+            {
+                StopAllCoroutines();
+
+                _animator.SetTrigger("React");
+                _animator.SetBool("Idle", false);
+                _rigidbody.velocity = Vector2.zero;
+                _isReacting = true;
+            }
+            else
+            {
+                if (!_animator.GetCurrentAnimatorStateInfo(0).IsTag("React"))
+                {
+                    _isChasing = true;
+                    _isReacting = false;
+                    speed *= 2;
+                }
+            }
+        }
+
+        if (_isChasing )
+        {
+            if (!_isAttacking && Vector2.Distance(transform.position, _player.position) < 1f)
+            {
+                _animator.SetTrigger("Attack");
+                _rigidbody.velocity = Vector2.zero;
+                _isAttacking = true;
+                transform.position = new Vector2(transform.position.x, transform.position.y + 0.11f);
+            }
+            else if (!_isAttacking && Vector2.Distance(transform.position, _player.position) < chaseRange)
+            {
+                Vector2 direction = _player.position - transform.position;
+
+                _animator.SetBool("Idle", false);
+                _rigidbody.velocity = new Vector2(direction.normalized.x * speed, _rigidbody.velocity.y);
+            }
+            else if(!_isAttacking)
+            {
+                speed *= 0.5f;
+                _isChasing = false;
+                UpdateDirection();
+                StartCoroutine("PatrolToTarget");
+            }
+            
+        }
+
+        if(_isAttacking && !_animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            _isAttacking = false;
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.11f);
+        }
+
+    }
+
+    private void LateUpdate()
+    { 
+        if (_isChasing)
+        {
+            if(_player.position.x >= transform.position.x)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+    }
+
+    IEnumerator PatrolToTarget()
+    {
+        while (Vector2.Distance(transform.position, _target.transform.position) > 0.25f)
+        {            
+            Vector2 direction = _target.transform.position - transform.position;
+
+            _animator.SetBool("Idle", false);
+            _rigidbody.velocity = new Vector2(direction.normalized.x * speed, _rigidbody.velocity.y);
+            yield return null;
+        }
+
+        transform.position = new Vector2(_target.transform.position.x, transform.position.y);
+        _rigidbody.velocity = Vector2.zero;
+        UpdateDirection();
+
+        _animator.SetBool("Idle", true);
+
+        yield return new WaitForSeconds(waitingTime);
+
+        StartCoroutine("PatrolToTarget");
+    }
+
+    private void UpdateDirection()
+    {
+        // First Time
+        if (_target == null)
+        {
+            _target = first;
+            if(transform.position.x <= first.transform.position.x)
+                transform.localScale = new Vector3(1, 1, 1);
+            else
+                transform.localScale = new Vector3(-1, 1, 1);
+
+            return;
+        }
+        
+        else if (_target.transform.position.x == first.transform.position.x)
+        {
+            _target = second;
+            transform.localScale = new Vector3(1, 1, 1);
+
+        }
+        else if (_target.transform.position.x == second.transform.position.x)
+        {
+            _target = first;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    public void moveInLadder(Transform endLadder)
+    {
+        if(!_isReacting && !_isChasing)
+        {
+            StopAllCoroutines();
+            _ladderTarget = endLadder;
+
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            StartCoroutine("MovingInLadder");
+        }
+    }
+
+    IEnumerator MovingInLadder()
+    {
+        while (Vector2.Distance(transform.position, _ladderTarget.transform.position) > 0.025f)
+        {
+            _animator.SetBool("Idle", true);
+
+            Vector2 direction = _ladderTarget.transform.position - transform.position;
+            transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+            yield return null;
+        }
+
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        yield return new WaitForSeconds(1f);
+        _animator.SetBool("Idle", false);
+
+        StartCoroutine("PatrolToTarget");
+    }
+
+
+}
