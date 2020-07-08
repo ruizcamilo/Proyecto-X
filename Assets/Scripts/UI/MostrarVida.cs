b75b1d488314ca7e5575b608bb92d8a535b0e390
JDevMen <jdmendoza_90@hotmail.com>
2020-07-08 13:25:00 -0500

    ver.1 mostrador de vida

---
+++ Assets/Scripts/UI/MostrarVida.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MostrarVida : MonoBehaviour
+{
+    private int puntosVida = 5;
+    public Text textoVida;
+
+     void Update()
+    {
+        textoVida.text = "VIDA: "+ puntosVida;
+
+        if (Input.GetKeyDown(KeyCode.Space)) puntosVida-- ;
+    }
+}
