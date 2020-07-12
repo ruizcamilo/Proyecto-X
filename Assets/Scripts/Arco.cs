985fa77dec478e475ea6a779a4505f1222a0c7aa
JDevMen <jdmendoza_90@hotmail.com>
2020-07-12 13:31:53 -0500

    Arquero ver.1.1, mejorado detección de pared

---
+++ Assets/Scripts/Arco.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arco : MonoBehaviour
+{
+    public Transform lanzadorDeObjetos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Se usa para disparar, por ahora se usa la tecla "0" del numpad
+        if (Input.GetButtonDown("[0]"))
+        {
+            dispararObjeto();
+        }   
+    }
+
+    void dispararObjeto()
+    {
+
+    }
+}
