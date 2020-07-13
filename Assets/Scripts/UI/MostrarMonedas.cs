6cdc6feb14c973b70e647e58263417d8b0f3d07a
JDevMen <jdmendoza_90@hotmail.com>
2020-07-13 00:34:04 -0500

    UI ver.1.1 UI, texto contador monedas

---
+++ Assets/Scripts/UI/MostrarMonedas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MostrarMonedas : MonoBehaviour
+{
+    private int totalMonedas = 0;
+    public Text textoMonedas ;
+
+    void Update()
+    {
+        textoMonedas.text = "MONEDAS: " + totalMonedas;
+
+        if (Input.GetKeyDown(KeyCode.RightAlt)) totalMonedas++;
+    }
+}
