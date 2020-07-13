a77561bbd2ac6609a1e92269e94cf4d3530088d6
JDevMen <jdmendoza_90@hotmail.com>
2020-07-13 12:11:58 -0500

    Integración UI con escena con jugador, falta conexión entre variables vida y monedas

---
+++ Assets/Scripts/UI/UI_Selector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Selector : MonoBehaviour
+{
+    private Transform casilla_Template;
+
+    private void Awake()
+    {
+        casilla_Template = transform.Find("Casilla_Template");
+        casilla_Template.gameObject.SetActive(false);
+
+    }
+
+    private void updateVisual()
+    {
+
+    }
+}
