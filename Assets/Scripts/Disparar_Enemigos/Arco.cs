07819232ce51d08201e74c6e5362e63fd30cfb02
Julian Mendoza <jdmendoza_90@hotmail.com>
2020-07-15 14:41:29 -0500

    arquero ver 1.3 arquero patrulla y lanzamiento automático terminado

--- Assets/Scripts/Disparar_Enemigos/Arco.cs
+++ Assets/Scripts/Disparar_Enemigos/Arco.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class Arco : MonoBehaviour
 {
+    public float timer = 2f ;
     public Transform lanzadorDeObjetos;
     public GameObject proyectil;
+    private bool shooting = false;
 
     // Update is called once per frame
     void Update()
     {
-        //Se usa para disparar, por ahora se usa la tecla "0" del numpad
-        if (Input.GetKeyDown("[0]"))
+        if (Input.GetKeyDown(KeyCode.Keypad0))
         {
             dispararObjeto();
-        }   
+        }
+        Debug.Log("Entrar al update");
+        //Dispara el proyectil cada n segundos.
+        if(shooting == false)
+        {
+            Debug.Log("Entro a shooting");
+            StartCoroutine(shootingTimerRoutine());
+        }
+    }
+
+    //Corutina para disparar el proyectil cada n segundos definido por el timer;
+    IEnumerator shootingTimerRoutine()
+    {
+        shooting = true;
+        dispararObjeto();
+        yield return new WaitForSecondsRealtime(timer);
+        shooting = false;
     }
 
     void dispararObjeto()
